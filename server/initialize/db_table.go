9403a1ad5630c0686bcff1be9bb244d54bdcae20
VGTsome <xuwang88@qq.com>
2020-07-20 16:27:44 +0800

    init

---
+++ server/initialize/db_table.go
@@ -0,0 +1,33 @@
+package initialize
+
+import (
+	"gin-vue-admin/global"
+	"gin-vue-admin/model"
+)
+
+// 注册数据库表专用
+func DBTables() {
+	db := global.GVA_DB
+	db.AutoMigrate(model.SysUser{},
+		model.SysAuthority{},
+		model.SysApi{},
+		model.SysBaseMenu{},
+		model.JwtBlacklist{},
+		model.SysWorkflow{},
+		model.SysWorkflowStepInfo{},
+		model.SysDictionary{},
+		model.SysDictionaryDetail{},
+		model.ExaFileUploadAndDownload{},
+		model.ExaFile{},
+		model.ExaFileChunk{},
+		model.ExaCustomer{},
+		model.SysOperationRecord{},
+		model.CabinetProduct{},
+		model.SmartStorageCabinet{},
+		model.SmartStorageDoor{},
+		model.SmartStorageOrder{},
+		model.SmartStorageMonitor{},
+		model.SmartStorageProduct{},
+	)
+	global.GVA_LOG.Debug("register table success")
+}
